d8be733150739ec3502bea8dbe9b1ec8bb7279d5
altairus <elnair.rnd@gmail.com>
2015-01-21 22:01:21 +0300

    added Timestamp provider

---
+++ timestamp.go
@@ -0,0 +1,134 @@
+package null
+
+import (
+	"encoding/json"
+	"fmt"
+	pq "github.com/lib/pq"
+	"reflect"
+	"strconv"
+	"time"
+)
+
+// Time is an even nuller nullable Time.
+// It does not consider zero values to be null.
+// It will decode to null, not zero, if null.
+type Timestamp struct {
+	pq.NullTime
+}
+
+// NewTime creates a new Time
+func NewTimestamp(t time.Time, valid bool) Timestamp {
+	return Timestamp{
+		NullTime: pq.NullTime{
+			Time:  t,
+			Valid: valid,
+		},
+	}
+}
+
+// TimeFrom creates a new Time that will always be valid.
+func TimestampFrom(t time.Time) Timestamp {
+	return NewTimestamp(t, true)
+}
+
+// TimeFromPtr creates a new Time that be null if i is nil.
+func TimestampFromPtr(t *time.Time) Timestamp {
+	if t == nil {
+		return NewTimestamp(time.Time{}, false)
+	}
+	return NewTimestamp(*t, true)
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+// It supports null.Time JSON or nil values
+// It also supports unmarshalling a pq.NullTime
+func (t *Timestamp) UnmarshalJSON(data []byte) error {
+	var err error
+	var v interface{}
+	var i struct {
+		Time int64
+	}
+	var j struct {
+		Time int64
+		Valid bool
+	}
+	json.Unmarshal(data, &v)
+	switch v.(type) {
+	case float64:
+		err = json.Unmarshal(data, &i.Time)
+		if err == nil {
+			t.Time = time.Unix(i.Time, 0)
+		}
+	case map[string]interface{}:
+		err = json.Unmarshal(data, &j)
+		if (err == nil) && j.Valid {
+			fmt.Println("======!=========!!=======", j)
+			t.Time = time.Unix(j.Time, 0)
+		}
+	case nil:
+		t.Valid = false
+		return nil
+	default:
+		err = fmt.Errorf("json: cannot unmarshal %v into Go value of type null.Timestamp", reflect.TypeOf(v).Name())
+	}
+	t.Valid = err == nil
+	return err
+}
+
+// UnmarshalText implements encoding.TextUnmarshaler.
+// It will unmarshal to a null Int if the input is a blank or not an time.Time RFC3339.
+// It will return an error if the input is not an time.Time RFC3339, blank, or "null".
+func (t *Timestamp) UnmarshalText(text []byte) error {
+	str := string(text)
+	if str == "" || str == "null" {
+		t.Valid = false
+		return nil
+	}
+	var (
+		err error
+		i   int64
+	)
+	i, err = strconv.ParseInt(str, 0, 64)
+	if err == nil {
+		t.Time = time.Unix(i, 0)
+	}
+	t.Valid = err == nil
+	return err
+}
+
+// MarshalJSON implements json.Marshaler.
+// It will encode time.Time RFC3339 or null if this Time is null
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	if !t.Valid {
+		return []byte("null"), nil
+	}
+	return []byte(strconv.FormatInt(t.Time.Unix(), 10)), nil
+}
+
+// MarshalText implements encoding.TextMarshaler.
+// It will encode a blank string if this Time is null.
+func (t Timestamp) MarshalText() ([]byte, error) {
+	if !t.Valid {
+		return []byte{}, nil
+	}
+	return []byte(strconv.FormatInt(t.Time.Unix(), 10)), nil
+}
+
+// SetValid changes this Time's value and also sets it to be non-null.
+func (t *Timestamp) SetValid(n time.Time) {
+	t.Time = n
+	t.Valid = true
+}
+
+// Ptr returns a pointer to this Time's value, or a nil pointer if this Time is null.
+func (t Timestamp) Ptr() *time.Time {
+	if !t.Valid {
+		return nil
+	}
+	return &t.Time
+}
+
+// IsZero returns true for invalid Times, for future omitempty support (Go 1.4?)
+func (t Timestamp) IsZero() bool {
+	return !t.Valid
+}
---
+++ timestamp_test.go
@@ -0,0 +1,192 @@
+package null
+
+import (
+	"encoding/json"
+	"math"
+	"strconv"
+	"testing"
+	"time"
+)
+
+var (
+	testTimestamp = time.Unix(1419196236, 0)
+	//nullJSON     = []byte(`null`)
+	timestampJSON     = []byte(`1419196236`)
+	nullTimestampJSON = []byte(`{"Time":1419196236,"Valid":true}`)
+)
+
+func testTimestampstampFrom(t *testing.T) {
+	i := TimestampFrom(testTimestamp)
+	assertTimestamp(t, i, "TimestampFrom()")
+
+	zero := TimestampFrom(time.Time{})
+	if !zero.Valid {
+		t.Error("TimestampFrom(0)", "is invalid, but should be valid")
+	}
+}
+
+func testTimestampstampFromPtr(t *testing.T) {
+	n := testTimestamp
+	iptr := &n
+	i := TimestampFromPtr(iptr)
+	assertTimestamp(t, i, "TimestampFromPtr()")
+
+	null := TimestampFromPtr(nil)
+	assertNullTimestamp(t, null, "TimestampFromPtr(nil)")
+}
+
+func TestUnmarshalTimestamp(t *testing.T) {
+	var i Timestamp
+	err := json.Unmarshal(timestampJSON, &i)
+	maybePanic(err)
+	assertTimestamp(t, i, "int json")
+
+	var ni Timestamp
+	err = json.Unmarshal(nullTimestampJSON, &ni)
+	maybePanic(err)
+	assertTimestamp(t, ni, "pq.NullTime json")
+
+	var null Timestamp
+	err = json.Unmarshal(nullJSON, &null)
+	maybePanic(err)
+	assertNullTimestamp(t, null, "null json")
+
+	var badType Timestamp
+	err = json.Unmarshal(boolJSON, &badType)
+	if err == nil {
+		panic("err should not be nil")
+	}
+	assertNullTimestamp(t, badType, "wrong type json")
+}
+
+func TestUnmarshalNonTimestampNumber(t *testing.T) {
+	var i Timestamp
+	err := json.Unmarshal(floatJSON, &i)
+	if err == nil {
+		panic("err should be present; non-integer number coerced to time.Time")
+	}
+}
+
+func TestUnmarshalTimestampOverflow(t *testing.T) {
+	int64Overflow := uint64(math.MaxInt64)
+
+	// Max int64 should decode successfully
+	var i Timestamp
+	err := json.Unmarshal([]byte(strconv.FormatUint(int64Overflow, 10)), &i)
+	maybePanic(err)
+
+	// Attempt to overflow
+	int64Overflow++
+	err = json.Unmarshal([]byte(strconv.FormatUint(int64Overflow, 10)), &i)
+	if err == nil {
+		panic("err should be present; decoded value overflows int64")
+	}
+}
+
+func TestTextUnmarshalTimestamp(t *testing.T) {
+	var i Timestamp
+	err := i.UnmarshalText(timestampJSON)
+	maybePanic(err)
+	assertTimestamp(t, i, "UnmarshalText() time.Time")
+
+	var blank Timestamp
+	err = blank.UnmarshalText([]byte(""))
+	maybePanic(err)
+	assertNullTimestamp(t, blank, "UnmarshalText() empty time.Time")
+
+	var null Timestamp
+	err = null.UnmarshalText([]byte("null"))
+	maybePanic(err)
+	assertNullTimestamp(t, null, `UnmarshalText() "null"`)
+}
+
+func TestMarshalTimestamp(t *testing.T) {
+	i := TimestampFrom(testTimestamp)
+	data, err := json.Marshal(i)
+	maybePanic(err)
+	assertJSONEquals(t, data, "1419196236", "non-empty json marshal")
+
+	// invalid values should be encoded as null
+	null := NewTimestamp(time.Time{}, false)
+	data, err = json.Marshal(null)
+	maybePanic(err)
+	assertJSONEquals(t, data, "null", "null json marshal")
+}
+
+func TestMarshalTimestampText(t *testing.T) {
+	i := TimestampFrom(testTimestamp)
+	data, err := i.MarshalText()
+	maybePanic(err)
+	assertJSONEquals(t, data, "1419196236", "non-empty text marshal")
+
+	// invalid values should be encoded as null
+	null := NewTimestamp(time.Time{}, false)
+	data, err = null.MarshalText()
+	maybePanic(err)
+	assertJSONEquals(t, data, "", "null text marshal")
+}
+
+func testTimestampstampPointer(t *testing.T) {
+	i := TimestampFrom(testTimestamp)
+	ptr := i.Ptr()
+	if *ptr != testTimestamp {
+		t.Errorf("bad %s int: %#v ≠ %s\n", "pointer", ptr, 12345)
+	}
+
+	null := NewTimestamp(time.Time{}, false)
+	ptr = null.Ptr()
+	if ptr != nil {
+		t.Errorf("bad %s time.Time: %#v ≠ %s\n", "nil pointer", ptr, "nil")
+	}
+}
+
+func testTimestampstampIsZero(t *testing.T) {
+	i := TimestampFrom(testTimestamp)
+	if i.IsZero() {
+		t.Errorf("IsZero() should be false")
+	}
+
+	null := NewTimestamp(time.Time{}, false)
+	if !null.IsZero() {
+		t.Errorf("IsZero() should be true")
+	}
+
+	zero := NewTimestamp(time.Time{}, true)
+	if zero.IsZero() {
+		t.Errorf("IsZero() should be false")
+	}
+}
+
+func testTimestampstampSetValid(t *testing.T) {
+	change := NewTimestamp(time.Time{}, false)
+	assertNullTimestamp(t, change, "SetValid()")
+	change.SetValid(testTimestamp)
+	assertTimestamp(t, change, "SetValid()")
+}
+
+func testTimestampstampScan(t *testing.T) {
+	var i Timestamp
+	err := i.Scan(testTimestamp)
+	maybePanic(err)
+	assertTimestamp(t, i, "scanned time.Time")
+
+	var null Timestamp
+	err = null.Scan(nil)
+	maybePanic(err)
+	assertNullTimestamp(t, null, "scanned null")
+}
+
+func assertTimestamp(t *testing.T, i Timestamp, from string) {
+	if i.Time != testTimestamp {
+		t.Errorf("bad %s time.Time: %d ≠ %d\n", from, i.Time, testTimestamp)
+	}
+	if !i.Valid {
+		t.Error(from, "is invalid, but should be valid")
+	}
+}
+
+func assertNullTimestamp(t *testing.T, i Timestamp, from string) {
+	if i.Valid {
+		t.Error(from, "is valid, but should be invalid")
+	}
+}
